9d230ac690567263de52eeff39bef711dcae8776
Abhishek Gupta <75923437+guptaji9630@users.noreply.github.com>
2022-08-25 18:32:51 +0530

    Update Extrastorage.sol

--- Extrastorage.sol
+++ Extrastorage.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.8;
 
 import "./SimpleStorage.sol";
 
-contract ExtraStorage is SimpleStorage {
+contract ExtraStorage is SimpleStorage {            // There are two types of override a variable in afunction like in this for overriding in this first you need to type virtual in simple storage crantract then with override in this Extrastorage contact.
     function store(uint256 _favoriteNumber) public override {
         favoriteNumber = _favoriteNumber + 5;
     }
